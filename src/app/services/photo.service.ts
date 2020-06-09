200b6b2a813ec71d8eb5fcb2db17e6c3ee033768
Blanchi <mononokeee@yahoo.es>
2020-06-09 10:41:23 +0200

    add to gallery

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Plugins, CameraResultType, Capacitor, FilesystemDirectory, CameraPhoto, CameraSource } from '@capacitor/core';
 
-const {Camera, Filesystem, Storage } = Plugins;
+const { Camera, Filesystem, Storage } = Plugins;
 
 @Injectable({
   providedIn: 'root'
 })
 export class PhotoService {
 
-  constructor() { 
-    //this is nothing 
+  constructor() {}
+
+  public async addNewToGallery(){
+    const capturedPhoto = await Camera.getPhoto({
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+      quality: 100
+    });
   }
+
+
 }
